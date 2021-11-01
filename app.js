512e0f9d6328619913b3421bf9fc3f979be96b36
cangussu12 <leo.tywin@gmail.com>
2021-11-01 17:23:19 -0300

    inclusão do jquery com uma condição

--- app.js
+++ app.js
@@ -61,9 +61,27 @@ function cadastrarDespesa() {
         descricao.value, 
         valor.value
     )
-    if(despesa.validarDados()) {
-        console.log('Dados ok')
-    } else {
-        console.log('Dados invalidos')
-    }
+
+	if(despesa.validarDados()) {
+		//bd.gravar(despesa)
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		//dialog de sucesso
+		$('#modalRegistraDespesa').modal('show') 
+	} else {
+		
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		//dialog de erro
+		$('#modalRegistraDespesa').modal('show') 
+	}
 }
