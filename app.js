00fb5165faa1fc56e14529a38eed1aac41b9b91b
cangussu12 <leo.tywin@gmail.com>
2021-11-03 15:53:50 -0300

    ajustes finais

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ class Bd {
 				continue
 			}
 
+			despesa.id = i
 			despesas.push(despesa)
 		}
 
@@ -112,6 +113,10 @@ class Bd {
 		
 		return despesasFiltradas
 
+	}
+
+	remover(id) {
+		localStorage.removeItem(id)
 	}
 }
 
@@ -174,18 +179,6 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
     if(despesas.length == 0 && filtro == false){
 		despesas = bd.recuperarTodosRegistros() 
 	}
-	
-
-	/*
-
-	<tr>
-		<td>15/03/2018</td>
-		<td>Alimentação</td>
-		<td>Compras do mês</td>
-		<td>444.75</td>
-	</tr>
-
-	*/
 
 	let listaDespesas = document.getElementById("listaDespesas")
     listaDespesas.innerHTML = ''
@@ -214,7 +207,21 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 		linha.insertCell(1).innerHTML = d.tipo
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
-		console.log(d)
+		
+
+		//criar o botão de exclusão
+		let btn = document.createElement('button')
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = `id_despesa_${d.id}`
+		btn.onclick = function() {
+			//remover a despesa
+			let id = this.id.replace('id_despesa_', '')
+			bd.remover(id)
+
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
 	})
 
  }
